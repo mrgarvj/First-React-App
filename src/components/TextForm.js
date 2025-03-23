c2b98f99eeeb3a02ec109d4f2f172245938e3713
garvjangra2009123 <localsp.reviews.corp@gmail.com>
2025-03-24 00:33:18 +0530

    New Changes added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -9,15 +9,22 @@ export default function TextForm(props) {
     const handleOnChange = (event) => {
         // console.log("On Change")
         setText(event.target.value)
-    }
+    }    
     const [text, setText] = useState('Enter text here');
     return (
         <>
+        <div className='container'>
         <h1>{props.heading}</h1>
         <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id="MyBox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary" onClick={handleUpClick} >Convert to upper case</button>
+        </div>
+        <div className="container my-3">
+            <h1>Your Text Summary</h1>
+            <p>{text.split(" ").length} Words and {text.length} characters(included spaces)</p>
+            <p>{text.split(" ").length * 0.008} Minutes to read</p>
+        </div>
         </>
     )
 }
